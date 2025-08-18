96325bf621846215a69154c1004ef208641d8779
Andrul01 <andrumorallies@gmail.com>
2025-08-19 00:36:31 +0530

    Implement CRUD functionality for Employee model and enhance templates for user interaction

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,13 +14,40 @@ class Employee(db.Model):
     def __repr__(self) -> str:
         return f"{self.id} - {self.email}"
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    emp = Employee(email="andrul@gmail.com", password="123456")
-    db.session.add(emp)
-    db.session.commit()
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # print(f"Email: {email}, Password: {password}")
+        
+        emp = Employee(email=email, password=password)
+        db.session.add(emp)
+        db.session.commit()
     employees = Employee.query.all()
     return render_template('index.html', employees=employees)
 
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        emp = Employee.query.filter_by(id=id).first()
+        emp.email = email
+        emp.password = password
+        db.session.commit()
+        return redirect('/')
+    emp = Employee.query.filter_by(id=id).first()   
+    return render_template('edit.html', emp=emp)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    emp = Employee.query.filter_by(id=id).first()
+    db.session.delete(emp)
+    db.session.commit()
+    return redirect('/')
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
